031e08e69a84b78fb34b9a0398b2c649395776a6
rwjohns1 <rwjohns1@uno.edu>
2022-07-29 20:35:39 -0500

    really adding the skeleton tester this time

---
+++ HW5/MyGraphTester.java
@@ -0,0 +1,83 @@
+/**
+ * Author - Robin Johnson
+ * Verion - 1.0
+ * 
+ * Discription:
+ *             Tester Class to test the assigned methods
+ * 			   
+ * 				Tests the following methods:
+ * 				
+ * 			
+ * 			
+ * 			
+ */
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyGraphTester{
+
+
+	
+	/**
+	 * Method for setting up the enviorment of the tests
+	 * 
+	 */
+	@Before
+	public void setUp(){
+
+	}
+	/**
+	 * Method for testing Map.put(KeyType key, ValueType val)
+	 * 
+	 */
+	@Test
+	public void testMyGraph(){
+
+	}
+	/**
+	 * Method for testing Map.get(KeyType key)
+	 * 
+	 */
+	@Test
+	public void testAddVertex(){
+
+		
+
+	}
+	/**
+	 * Method for testing Map.isEmpty()
+	 * 
+	 */
+	@Test
+	public void testAddEdgeByObject(){
+
+	}
+	/**
+	 * Method for testing Map.isEmpty()
+	 * 
+	 */
+	@Test
+	public void testAddEdgeByName(){
+
+	}
+	/**
+	 * Method for testing Map.isEmpty()
+	 * 
+	 */
+	@Test
+	public void testRemoveVertexByObject(){
+
+	}
+	/**
+	 * Method for testing Map.makeEmpty()
+	 * 
+	 */
+	@Test
+	public void testRemoveVertextByName(){
+
+	}
+}
